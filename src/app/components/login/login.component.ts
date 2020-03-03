39b78df394626195093de22adb457dd9de1a6b89
Morgan Christopher Allen <morgan.allen@seagate.com>
2020-03-03 12:49:40 -0700

    added logo

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UserCredentials } from 'src/app/models/usercredentials';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
