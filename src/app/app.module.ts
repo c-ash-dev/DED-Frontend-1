1df8df1985d1bdae7f215c3d96c7744d1e544701
Alex Costinescu <paul.costinescu@gmail.com>
2020-03-31 14:36:57 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AuthGuard } from './services/authguard';
+import { AuthGuard } from './services/authentication.guard';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
