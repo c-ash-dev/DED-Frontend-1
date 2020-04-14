62d9dcb18895b0c0afc446023edb7293f47a6a60
Alex Costinescu <paul.costinescu@gmail.com>
2020-04-13 23:41:24 -0400

    Removed bad auto-generated tests

--- src/app/components/createworkout/createworkout.component.spec.ts
+++ src/app/components/createworkout/createworkout.component.spec.ts
@@ -18,8 +18,4 @@ describe('CreateworkoutComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/components/track/track.component.spec.ts
+++ src/app/components/track/track.component.spec.ts
@@ -18,8 +18,4 @@ describe('TrackComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
