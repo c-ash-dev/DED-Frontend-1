928458db4609c79d01829c36aa6f770ecedc49d4
Alex Costinescu <paul.costinescu@gmail.com>
2020-04-21 20:27:46 -0400

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['ChromeHeadless', 'TestChromeHeadless'],
+    browsers: ['TestChromeHeadless'],
     singleRun: true,
     restartOnFileChange: true,
     customLaunchers: {
